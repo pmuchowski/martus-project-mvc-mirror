947ad4777289d1cbb7a67454a51f14092f39463d
Nima A <nimaa@benetech.org>
2017-07-06 11:17:26 -0400

    Update from scott while debugging unsigned jar build failures

--- martus-build/JenkinsBuildUnsigned.sh
+++ martus-build/JenkinsBuildUnsigned.sh
@@ -2,4 +2,5 @@ export NSIS_HOME="$HOME/.wine/drive_c/Program Files/NSIS/"
 export ATTIC_DIR="/var/lib/jenkins/martus-client/builds/$BUILD_NUMBER/"
 sh $WORKSPACE/martus-build/checkout.sh
 hg log -l 1 -R martus-common
+pwd; env | sort > /var/tmp/jenkins.env; set -x
 buildr -f $WORKSPACE/martus-build/buildfile clean martus-client:build_unsigned test=no
