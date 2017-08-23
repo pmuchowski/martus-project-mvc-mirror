c3df549bd1774a94e85b272d3f9c7257b813ea9f
Pawel Muchowski <pmuchowski@soldevelo.com>
2017-08-23 17:28:42 +0200

    MARTUSDEV-1983: Create pure fx ModelessBusyDlg

---
+++ martus-client/source/org/martus/client/swingui/dialogs/PureFxModelessBusyDlg.java
@@ -0,0 +1,19 @@
+package org.martus.client.swingui.dialogs;
+
+import javafx.scene.image.Image;
+
+public class PureFxModelessBusyDlg implements ModelessBusyDlg
+{
+	public PureFxModelessBusyDlg(Image image)
+	{
+	}
+
+	public PureFxModelessBusyDlg(String message)
+	{
+	}
+
+	@Override
+	public void endDialog()
+	{
+	}
+}
