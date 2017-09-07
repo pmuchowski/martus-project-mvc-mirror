e4195c4ba4ea3ce69cdab51682c3687bd8206d32
Pawel Muchowski <pmuchowski@soldevelo.com>
2017-09-07 18:25:25 +0200

    MARTUSDEV-1983: Create SwingInFxPrintPreviewDlg

---
+++ martus-client/source/org/martus/client/swingui/dialogs/SwingInFxPrintPreviewDlg.java
@@ -0,0 +1,78 @@
+package org.martus.client.swingui.dialogs;
+
+import javax.swing.JComponent;
+
+import org.martus.client.reports.ReportOutput;
+import org.martus.client.swingui.UiMainWindow;
+
+public class SwingInFxPrintPreviewDlg extends SwingInFxDialog implements PreviewDlgInterface
+{
+	public SwingInFxPrintPreviewDlg(UiMainWindow mainWindowToUse, ReportOutput outputToUse)
+	{
+		mainWindow = mainWindowToUse;
+		output = outputToUse;
+
+		createAndSetSwingContent();
+	}
+
+	@Override
+	public JComponent createSwingContent()
+	{
+		previewDlg = new PrintPreviewDlg(mainWindow, output);
+		return previewDlg.getMainContent();
+	}
+
+	class PrintPreviewDlg extends UiPrintPreviewDlg
+	{
+		public PrintPreviewDlg(UiMainWindow mainWindowToUse, ReportOutput output)
+		{
+			super(mainWindowToUse, output);
+		}
+
+		public void dispose()
+		{
+			closeDialog();
+		}
+	}
+
+	@Override
+	public int getDialogWidth()
+	{
+		return 700;
+	}
+
+	@Override
+	public int getDialogHeight()
+	{
+		return 800;
+	}
+
+	@Override
+	public boolean wantsPrintToDisk()
+	{
+		return previewDlg.wantsPrintToDisk();
+	}
+
+	@Override
+	public boolean wantsExportToCsv()
+	{
+		return previewDlg.wantsExportToCsv();
+	}
+
+	@Override
+	public boolean wasCancelButtonPressed()
+	{
+		return previewDlg.wasCancelButtonPressed();
+	}
+
+	@Override
+	public void showDialog()
+	{
+		showAndWait();
+	}
+
+	private UiMainWindow mainWindow;
+	private ReportOutput output;
+
+	private UiPreviewDlg previewDlg;
+}
